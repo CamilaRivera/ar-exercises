d0bbc118645933559ad037df826123b552f6ca39
CamilaRivera <camila.riverart@gmail.com>
2019-10-15 05:32:37 +0000

    exercise 7 done

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates_length_of :name, minimum: 3
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :stores_contains_one_apparel
 
+  def stores_contains_one_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:apparel_type, "mens_apparel and womens_apparel cannot be false at the same time")
+    end
+  end
 end
